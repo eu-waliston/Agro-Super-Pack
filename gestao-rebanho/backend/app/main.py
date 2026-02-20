d43d2c9a841c6609c52160c8eb59c27a93b81e49
Waliston <wesantosdev@gmail.com>
2026-02-19 22:05:42 -0300

    criação de rota HOME para a API com informações relevantes

--- gestao-rebanho/backend/app/main.py
+++ gestao-rebanho/backend/app/main.py
@@ -1,11 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.database import Base, engine
 from app.routes import animal, registro_peso
 
-app = FastAPI(title="Gestão de Rebanho API")
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="Gestão de Rebanho API",version="1.0.0")
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(animal.router)
 app.include_router(registro_peso.router)
+
+
+# Templates
+templates = Jinja2Templates(directory="templates")
+
+# Arquivos estáticos (images, css, etc)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Endpoint de status
+app.get("/status")
+def status():
+    return {"status": "online"}
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "title": app.title,
+            "version": app.version
+        }
+    )
+
 
---
+++ gestao-rebanho/environment-guide.md
@@ -0,0 +1,17 @@
+# ⚡ 1. Criar o ambiente virtual
+
+```bash
+python -m venv venv
+```
+
+## 🔥 2. Ativar o ambiente [🐧Linux]
+
+```bash
+source venv/bin/activate
+```
+
+## 📦 3. Instalar bibliotecas
+
+```bash
+ pip install -r requirements.txt
+```
